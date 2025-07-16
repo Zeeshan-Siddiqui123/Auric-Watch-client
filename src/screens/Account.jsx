73fd4a1e63487a0f30ab5ad3740780a8887ec7be
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2025-07-16 17:07:14 +0500

    commit changes

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { API } from '../../API';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Account = () => {
   const [data, setData] = useState({
