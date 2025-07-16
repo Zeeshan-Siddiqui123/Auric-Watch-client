c98fee3742c71242d73d21301f605b3ce64ec981
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2025-07-16 18:34:49 +0500

    commit changes

--- src/screens/ResetPass.jsx
+++ src/screens/ResetPass.jsx
@@ -63,7 +63,7 @@ const ResetPass = () => {
                         <input
                             type={showPassword ? 'text' : 'password'}
                             placeholder="New Password"
-                            className="px-4 py-2 rounded-md bg-gray-900 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="w-full px-4 py-2 rounded-md bg-gray-900 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             value={newPassword}
                             onChange={(e) => setNewPassword(e.target.value)}
                             required
